6a2275d2f18c27ea3a252f16deaab583c367872f
theussoares <matheusfs774@gmail.com>
2026-04-11 02:41:09 -0400

    feat: add useBodyScrollLock shared hook

---
+++ src/shared/hooks/useBodyScrollLock.ts
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+export function useBodyScrollLock(locked: boolean) {
+  useEffect(() => {
+    if (locked) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [locked]);
+}
